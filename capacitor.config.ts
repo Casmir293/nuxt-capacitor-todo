edbae09961dadd71cd13c7b43dbd632b38b3efce
Casmir Onyeka <casmir293@gmail.com>
2025-09-10 14:51:34 +0100

    feat: move web files to web folder

--- capacitor.config.ts
+++
@@ -1,9 +0,0 @@
-import type { CapacitorConfig } from '@capacitor/cli';
-
-const config: CapacitorConfig = {
-  appId: 'dev.casmir.nuxtcaptodo',
-  appName: 'nuxt-cap-todo',
-  webDir: '.output/public'
-};
-
-export default config;
